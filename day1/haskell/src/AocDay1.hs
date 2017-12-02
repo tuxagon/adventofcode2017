103a9a8d742a851afddfa1f691ce9de0fd461c14
Kenneth Bogner <tuxagon91@gmail.com>
2017-12-01 23:03:47 -0500

    :sparkles: Added solution to day 1 question 1

--- day1/haskell/src/AocDay1.hs
+++ day1/haskell/src/AocDay1.hs
@@ -1,6 +1,24 @@
 module AocDay1
-    ( someFunc
+    ( puzzle1
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (digitToInt)
+import Prelude hiding (sum)
+
+puzzle1 :: String -> Int 
+puzzle1 []     = 0
+puzzle1 (x:xs) =
+  let count = snd $ foldl sum (x, 0) xs
+  in count + firstLast
+  where
+    firstLast = 
+      if x == l then 
+        digitToInt l 
+      else 
+        0
+      where l = last xs
+    sum (ch, acc) y = 
+      (y, if ch == y then 
+            acc + digitToInt y 
+          else 
+            acc)
