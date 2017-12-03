a517dd658869c6a3ee56b5bb2bbbe58556b02e19
Kenneth Bogner <tuxagon91@gmail.com>
2017-12-02 21:58:39 -0500

    :sparkles: Solved day 2 problem 2

--- haskell/src/Aoc.hs
+++ haskell/src/Aoc.hs
@@ -2,17 +2,18 @@ module Aoc
     ( d1puzzle1
     , d1puzzle2
     , d2puzzle1
+    , d2puzzle2
     ) where
 
 import Data.Array (Array, listArray, (!))
 import Data.Char (digitToInt)
+import Data.Maybe (fromMaybe)
 
 d1puzzle1 :: String -> Int 
 d1puzzle1 [] = 0
 d1puzzle1 xs =
-  snd $ foldl sum' (end, 0) xs
+  snd $ foldl sum' (last xs, 0) xs
   where
-    end = last xs
     sum' (ch, acc) y = 
       (y, if ch == y then 
             acc + digitToInt y 
@@ -42,3 +43,16 @@ d2puzzle1 []    = 0
 d2puzzle1 sheet =
   sum $ map diff sheet
   where diff row = maximum row - minimum row
+
+d2puzzle2 :: [[Int]] -> Int
+d2puzzle2 []   = 0
+d2puzzle2 sheet =
+  sum $ map evenlyDivide sheet
+  where
+    evenlyDivide row = 
+      foldl (\x acc -> if acc == 0 then x else acc) 0 $ map (divide row) row
+    divide row n = fromMaybe 0 $ case result of
+      [] -> Nothing
+      x -> Just $ last x `div` n 
+      where greater = filter (> n) row
+            result = filter (\x -> x `mod` n == 0) greater
